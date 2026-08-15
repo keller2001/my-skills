7497df95567aa3481971b59be1332e8e20e7ab74
Matrix-Node <matrix-node@skills-manager.local>
2026-08-15 11:25:28 +0800

    auto backup
    Skills-Manager-Protocol: 2

--- manage-personal-writing/scripts/rich-editor/launch.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0launch.ps1" %*
-set "exitCode=%ERRORLEVEL%"
-if not "%exitCode%"=="0" pause
-endlocal & exit /b %exitCode%
